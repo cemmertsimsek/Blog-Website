141935e84f39d761165fc4f1f3a0594220560be6
cem <cemsimseksm@gmail.com>
2022-10-25 20:13:52 +0300

    app.js file updated

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash"); //lodash used for _.lowerCase method
 
-let posts = []; // empty array for storing posts
-
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -20,6 +18,8 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+let posts = []; // empty array for storing posts
 
 app.get("/", function (req, res) {
   res.render("home", {
@@ -48,6 +48,7 @@ app.post("/compose", function (req, res) {
   };
 
   posts.push(post);
+
   res.redirect("/");
 });
 
@@ -55,12 +56,12 @@ app.post("/compose", function (req, res) {
 app.get("/posts/:postName", function (req, res) {
   const requestedTitle = _.lowerCase(req.params.postName); // storing requested title to compare later. using lodash => _.lowerCase method to store strings as lowercase
 
-  posts.forEach((post) => {
+  posts.forEach(function (post) {
     //loop through the each object and store the title name
 
     const storedTitle = _.lowerCase(post.title); // using lodash => _.lowerCase method to store strings as lowercase
 
-    if (requestedTitle === storedTitle) {
+    if (storedTitle === requestedTitle) {
       //comparing the titles in order to get the correct page
       res.render("post", {
         title: post.title,
